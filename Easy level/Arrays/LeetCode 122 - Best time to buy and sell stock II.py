b87a6ae01b851a992f0d522e608d87ca4fc1f3a2
Pratiksha Agrawal <agrawalprateeksha@gmail.com>
2021-08-31 23:25:45 -0400

    Modified

--- Easy level/Arrays/LeetCode 122 - Best time to buy and sell stock II.py
+++ Easy level/Arrays/LeetCode 122 - Best time to buy and sell stock II.py
@@ -1,14 +1,9 @@
 """
 You are given an array prices where prices[i] is the price of a given stock on the ith day.
-
 Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
-
 Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
 
-
-
 Example 1:
-
 Input: prices = [7,1,5,3,6,4]
 Output: 7
 Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
