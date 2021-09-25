fea66527b76c05f51eb5b2bbad72f16da0073286
Pratiksha Agrawal <agrawalprateeksha@gmail.com>
2021-09-25 10:23:18 -0400

    Added to Medium Arrays

---
+++ Medium level/Arrays/LeetCode 289 - Game of life.py
@@ -0,0 +1,57 @@
+"""
+According to Wikipedia's article: "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
+
+The board is made up of an m x n grid of cells, where each cell has an initial state:
+live (represented by a 1) or dead (represented by a 0). Each cell interacts with its eight neighbors (horizontal, vertical, diagonal)
+using the following four rules (taken from the above Wikipedia article):
+
+Any live cell with fewer than two live neighbors dies as if caused by under-population.
+Any live cell with two or three live neighbors lives on to the next generation.
+Any live cell with more than three live neighbors dies, as if by over-population.
+Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+The next state is created by applying the above rules simultaneously to every cell in the current state, where births and deaths occur simultaneously. Given the current state of the m x n grid board, return the next state.
+
+Example 1:
+Input: board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]
+Output: [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]
+
+Follow up:
+Could you solve it in-place? Remember that the board needs to be updated simultaneously:
+You cannot update some cells first and then use their updated values to update other cells.
+In this question, we represent the board using a 2D array. In principle, the board is infinite,
+ which would cause problems when the active area encroaches upon the border of the array (i.e., live cells reach the border). How would you address these problems?
+"""
+
+# Using different notations for newly alive and newly dead
+# Time: O(MN)
+# Space: O(1)
+
+class Solution:
+    def gameOfLife(self, board):
+
+        neighbors = [(1,0), (0,1), (-1,0), (0,-1), (-1,-1), (1,1)]
+
+        rows = len(board)
+        cols = len(board[0])
+
+        for row in rows:
+            for col in cols:
+                live_neighbors = 0
+                for n in neighbors:
+                    r = row + n[0]
+                    c = col + n[1]
+                    if rows > r >= 0 and cols > c >= 0 and abs(board[r][c]) == 1:
+                        live_neighbors += 1
+                if board[row][col] == 1 and (live_neighbors<2 or live_neighbors>3):
+                    board[row][col] = -1
+                elif board[row][col] == 0 and live_neighbors == 3:
+                    board[row][col] == 2
+        # board = [[1 if board[row][col]>1 else 0 for col in range(cols)] for row in range(rows)]
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] > 0:
+                    board[row][col] = 1
+                else:
+                    board[row][col] = 0
+
+
